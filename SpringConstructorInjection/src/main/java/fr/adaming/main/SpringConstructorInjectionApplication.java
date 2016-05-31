8c83d27610241f8ceb154addb58b74debcb809a9
RaphaelFOURQUET <raphikifourque@gmail.com>
2016-05-31 09:54:38 +0200

    TP 15 : Mapping Object/XML

--- SpringConstructorInjection/src/main/java/fr/adaming/main/SpringConstructorInjectionApplication.java
+++ SpringConstructorInjection/src/main/java/fr/adaming/main/SpringConstructorInjectionApplication.java
@@ -1,5 +1,7 @@
 package fr.adaming.main;
 
+import java.io.IOException;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,84 +19,110 @@ import fr.adaming.services.elexpression.CustomerEL;
 import fr.adaming.services.elexpression.CustomerMethodinvocation;
 import fr.adaming.services.regularexpression.CustomerRegularExpression;
 import fr.adaming.services.ternaire.CustomerTernaire;
+import fr.adaming.services.toxml.CustomerToXml;
+import fr.adaming.services.toxml.XMLConverter;
 
 @SpringBootApplication
 public class SpringConstructorInjectionApplication {
+	
+	private static final String XML_FILE_NAME = "src/main/resources/customerToXml.xml";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//SpringApplication.run(SpringConstructorInjectionApplication.class, args);
-		
+
 		//Recuperation des Beans
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("Spring-Customer.xml");
-		
-//		<!-- Injection par constructeur 3 parametres -->
+
+		//		<!-- Injection par constructeur 3 parametres -->
 		Personne p = (Personne)context.getBean("PersonneBean");
 		System.out.println(p);
-		
-//		<!-- Injection par constructeur sans parametres et utilisation des setteurs -->
+
+		//		<!-- Injection par constructeur sans parametres et utilisation des setteurs -->
 		Customer c = (Customer) context.getBean("CustomerBean");
 		System.out.println(c);
-		
-		
+
+
 		CustomerService custA = (CustomerService)context.getBean("customerService");
 		custA.setMessage("Message by custA");
 		System.out.println("Message : " + custA.getMessage());
-		
+
 		//retrieve it again
 		CustomerService custB = (CustomerService)context.getBean("customerService");
 		System.out.println("Message : " + custB.getMessage());
-		
+
 		//collections
 		CustomerCollection custCollection = (CustomerCollection) context.getBean("CustomerCollectionBean");
 		System.out.println(custCollection);
 		//recup from collection Map List
 		CustomerRecupFromMapList customerRecupFromMapList = (CustomerRecupFromMapList) context.getBean("customerRecupFromMapListBean");
 		System.out.println(customerRecupFromMapList);
-		
-//		<!-- Bean par héritage -->
+
+		//		<!-- Bean par héritage -->
 		CustomerHeritage custHeritage = (CustomerHeritage)context.getBean("CustomerHeritageBean");
 		System.out.println(custHeritage);
-//		<!-- Override country -->
+		//		<!-- Override country -->
 		CustomerHeritage custHeritage2 = (CustomerHeritage)context.getBean("CustomerHeritageBean2");
 		System.out.println(custHeritage2);
-		
-//		<!-- Cycle de vie Bean par init-method and destroy-method dans le xml : autre solution par Spring @PostConstruct And @PreDestroy (mais dependances Spring) -->
+
+		//		<!-- Cycle de vie Bean par init-method and destroy-method dans le xml : autre solution par Spring @PostConstruct And @PreDestroy (mais dependances Spring) -->
 		CustomerLifeCycle customerLifeCycle = (CustomerLifeCycle) context.getBean("CustomerLifeCycle");
 		System.out.println(customerLifeCycle);
-		
-//		<!-- Spring EL Expression : On peut egalement passer par annotations -->
+
+		//		<!-- Spring EL Expression : On peut egalement passer par annotations -->
 		CustomerEL customerEL = (CustomerEL) context.getBean("customerElBean");
 		System.out.println(customerEL);
-		
-//		<!-- Spring EL Expression  par annotations --> prb : Une seule annotation @Component possible
+
+		//		<!-- Spring EL Expression  par annotations --> prb : Une seule annotation @Component possible
 		CustomerEL customerEL2 = (CustomerEL) context.getBean("customerElBean2");
 		System.out.println(customerEL2);
-		
-//		Spring EL method invocation
+
+		//		Spring EL method invocation
 		CustomerMethodinvocation customerMethodinvocation = (CustomerMethodinvocation) context.getBean("customerMethodInvocationBean");
 		System.out.println(customerMethodinvocation);
-		
-//		<!-- Operateur ternaire -->
+
+		//		<!-- Operateur ternaire -->
 		CustomerTernaire customerTernaire = (CustomerTernaire)context.getBean("customerTernaireBean");
 		System.out.println(customerTernaire);
-		
+
 		//Expression Reguliere
 		CustomerRegularExpression customerRegularExpression = (CustomerRegularExpression)context.getBean("customerRegExBean");
 		System.out.println(customerRegularExpression);
-		
+
 		//Auto scanning : no bean in xml nor @Component("name")
 		CustomerAutoScanning customerAutoScanning = (CustomerAutoScanning)context.getBean("customerAutoScanning");
 		System.out.println(customerAutoScanning);
-		
-//		<!-- Auto Wiring By name-->
+
+		//		<!-- Auto Wiring By name-->
 		CustomerAutoWiringByName customerAutoWiringByName = (CustomerAutoWiringByName)context.getBean("customerAutoWiringByNameBean");
 		System.out.println(customerAutoWiringByName);
-		
-//		<!-- AutoWiring by annotation -->
+
+		//		<!-- AutoWiring by annotation -->
 		CustomerAutoWiringByAnnotation customerAutoWiringByAnnotation = (CustomerAutoWiringByAnnotation)context.getBean("customerAutoWiringByAnnotationBean");
 		System.out.println(customerAutoWiringByAnnotation);
+
+		//		<!-- XML conversion via Spring oxm  : genere customerToXml.xml et utilise mapping.xml -->
+		XMLConverter converter = (XMLConverter)context.getBean("XMLConverter");
 		
+		CustomerToXml customerToXml = new CustomerToXml();
+		customerToXml.setName("Raf");
+		customerToXml.setAge(26);
+		customerToXml.setFlag(true);
+		customerToXml.setAddress("This is address");
+		
+		System.out.println("Convert Object to XML!");
+		//from object to XML file
+		converter.convertFromObjectToXML(customerToXml, XML_FILE_NAME);
+		System.out.println("Done \n");
+		
+		
+		System.out.println("Convert XML back to Object!");
+		//from XML to object
+		CustomerToXml customerToXml2 = (CustomerToXml)converter.convertFromXMLToObject(XML_FILE_NAME);
+		System.out.println(customerToXml2);
+		System.out.println("Done");
+
+
 		context.close();
 	}
 }
